b5e6892e1e33b2ffb739da60f630918fbe105f89
ZEN <vk.hvng7@gmail.com>
2020-11-30 13:51:09 +0530

    updating /

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(
 app.options('*', cors());
 app.get('/',(req,res)=>{
     console.log(req.body)
-    res.send("IPL ECONOMY DATA SERVER \n /economy to see top 10 economical bowlers each year \n /economy/year  year=> 2010-2019  to see top 10 economical bowler of that year")
+    res.send("<h1>IPL ECONOMY DATA SERVER<h1> <p> /economy to see top 10 economical bowlers each year </p> <p>/economy/year  year=> 2010-2019  to see top 10 economical bowler of that year</p>")
 })
 
 
