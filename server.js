762bc40bf9c004376601d312e2e580cce18cf408
ZEN <vk.hvng7@gmail.com>
2020-11-30 13:32:52 +0530

    cors updated

--- server.js
+++ server.js
@@ -8,7 +8,13 @@ const app=express();
 
 app.use(express.static('public'));
 app.use(cors());
-
+app.use(
+    cors({
+        credentials: true,
+        origin: true
+    })
+);
+app.options('*', cors());
 app.get('/',(req,res)=>{
     console.log(req.body)
     res.send("express server test!!!!!!!!")
